fa31f0f955ed024ce00f6d8dd4ac8cf8ab609cee
Ebtihal Salim <124944456+Ebtihal09@users.noreply.github.com>
2023-07-27 14:53:33 +0300

    Add files via upload

---
+++ Task-2/dc_motor_1.ino
@@ -0,0 +1,25 @@
+// C++ code
+//
+int output1 = 7;
+int output2 = 6;
+
+void setup()
+{
+  pinMode(output1, OUTPUT);
+  pinMode(output2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(output1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(output2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  
+  
+  
+  
+  
+  
+}
